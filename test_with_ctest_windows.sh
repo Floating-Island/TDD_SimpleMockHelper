06ec7fbcee82395fd87eacdd59152748584e84b3
Alberto Mikulan <betomiku@gmail.com>
2023-05-01 03:14:17 +0200

    calling cmake ctest_test because ctest is not in system path.

--- test_with_ctest_windows.sh
+++ test_with_ctest_windows.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 cd builds/windows
-WINEPREFIX=/usr/wine/.wine xvfb-run -a wine ctest --progress --output-junit ../../junit_report.xml
+WINEPREFIX=/usr/wine/.wine xvfb-run -a wine cmake ctest_test --progress --output-junit ../../junit_report.xml
