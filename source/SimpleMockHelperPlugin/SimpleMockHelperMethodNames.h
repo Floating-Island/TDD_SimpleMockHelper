da29fb473f2e7affa0d32e47ae8b11f9b5927c78
Alberto Mikulan <betomiku@gmail.com>
2023-08-04 22:34:44 +0200

    testing different signature mocks in same global mock helper

--- source/SimpleMockHelperPlugin/SimpleMockHelperMethodNames.h
+++ source/SimpleMockHelperPlugin/SimpleMockHelperMethodNames.h
@@ -3,4 +3,5 @@ namespace MockedMethodNames
     const std::string ClassNameMethodName = std::string("ClassName::methodName");
     const std::string ClassNameOriginalMethod = std::string("ClassName::OriginalMethod");
     const std::string ClassNameOriginalOutput = std::string("ClassName::OriginalOutput");
+    const std::string ClassNameReturnsZero = std::string("ClassName::ReturnsZero");
 }
--- source/Tests/test_SimpleMockHelper.cpp
+++ source/Tests/test_SimpleMockHelper.cpp
@@ -33,6 +33,12 @@ public:
     {
         SIMPLEMOCKING_GLOBAL_EXECUTEMOCKED(std::string, MockedMethodNames::ClassNameOriginalOutput)
         return "the original output";
+    } 
+
+    int ReturnsZero()
+    {
+        SIMPLEMOCKING_GLOBAL_EXECUTEMOCKED(int, MockedMethodNames::ClassNameReturnsZero)
+        return 0;
     }  
 };
 
@@ -98,6 +104,62 @@ TEST(GlobalMockerTest, CheckOriginalOutputMock)
         std::cout << "GlobalMockHelper status: " << ((MockedGlobal::GlobalMockHelper.use_count() == 0) ? "expired" : "still in use") << std::endl;
 
         EXPECT_TRUE(something.OriginalOutput() == std::string("the mocked output!"));
+    }
+
+    std::cout << "GlobalMockHelper status (out of shared pointer scope): " << ((MockedGlobal::GlobalMockHelper.use_count() == 0) ? "expired" : "still in use") << std::endl;
+
+    std::cout << "execute OriginalOutput on something (global mock should be expired): " << something.OriginalOutput() << std::endl;
+
+    EXPECT_TRUE(something.OriginalOutput() == std::string("the original output"));
+}
+
+//global mock
+
+TEST(GlobalMockerDifferentSignaturesTest, CheckOriginalOutputMock)
+{
+    ClassName something = ClassName();
+
+    std::cout << "execute OriginalMethod on something (out of shared pointer scope, so not mocked): " << something.OriginalMethod() << std::endl;
+    
+    {    
+        std::cout << "GlobalMockHelper status: " << ((MockedGlobal::GlobalMockHelper.use_count() == 0) ? "expired" : "still in use") << std::endl;
+        
+        std::cout << "execute OriginalOutput on something (global mock should be expired): " << something.OriginalOutput() << std::endl;
+        
+        std::cout << "setting new GlobalMockHelper..." << std::endl;
+        
+        std::shared_ptr<SimpleMockHelper> aGlobalMockerHelper = std::make_shared<SimpleMockHelper>();
+        SimpleMockHelperInterface::SetGlobalMockHelper(aGlobalMockerHelper);
+        
+
+        std::function<std::string()> lambdaHolder2 = std::function<std::string()> ([]() -> std::string {return std::string("the mocked output!");});
+        
+        FunctionHolder<std::string()> MockedMethodHolder2 = FunctionHolder<std::string()>(lambdaHolder2);
+        
+        aGlobalMockerHelper->RegisterMock(MockedMethodNames::ClassNameOriginalOutput, &MockedMethodHolder2);
+        
+        
+        std::cout << "execute OriginalOutput on something (global mock should be in use now): " << something.OriginalOutput() << std::endl;
+        
+        std::cout << "GlobalMockHelper status: " << ((MockedGlobal::GlobalMockHelper.use_count() == 0) ? "expired" : "still in use") << std::endl;
+
+        EXPECT_TRUE(something.OriginalOutput() == std::string("the mocked output!"));
+
+
+
+
+        std::function<int> lambdaHolder3 = std::function<int> ([]() -> int {return 2;});
+        
+        FunctionHolder<int> MockedMethodHolder3 = FunctionHolder<int>(lambdaHolder3);
+        
+        aGlobalMockerHelper->RegisterMock(MockedMethodNames::ClassNameReturnsZero, &MockedMethodHolder3);
+        
+        
+        std::cout << "execute ReturnsZero on something (global mock should be in use now): " << something.ReturnsZero() << std::endl;
+        
+        std::cout << "GlobalMockHelper status: " << ((MockedGlobal::GlobalMockHelper.use_count() == 0) ? "expired" : "still in use") << std::endl;
+
+        EXPECT_TRUE(something.ReturnsZero() != 0);
     }
 
     std::cout << "GlobalMockHelper status (out of shared pointer scope): " << ((MockedGlobal::GlobalMockHelper.use_count() == 0) ? "expired" : "still in use") << std::endl;
