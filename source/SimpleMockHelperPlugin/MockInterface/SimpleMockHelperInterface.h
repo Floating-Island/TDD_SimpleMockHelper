1496e2207b54854e52f70a7d6275b831d06186bf
alber <betomiku@gmail.com>
2025-04-14 21:21:55 +0200

    Remove template argument list.

--- source/SimpleMockHelperPlugin/MockInterface/SimpleMockHelperInterface.h
+++ source/SimpleMockHelperPlugin/MockInterface/SimpleMockHelperInterface.h
@@ -30,8 +30,7 @@ const std::weak_ptr<SimpleMockHelper> GlobalMockHelperInstance = SimpleMockHelpe
     \
 if(!GlobalMockHelperInstance.expired() && GlobalMockHelperInstance.lock()->ContainsMethodToMock(typeid(ClassMethodAddress).name()))    \
 {   \
-    using methodTraits = SimpleMockHelper::MethodTraits<decltype(ClassMethodAddress)>;    \
-    return GlobalMockHelperInstance.lock()->ExecuteMockMethod<ReturnType, methodTraits::classType>(ClassMethodAddress, ##__VA_ARGS__);    \
+    return GlobalMockHelperInstance.lock()->ExecuteMockMethod(ClassMethodAddress, ##__VA_ARGS__);    \
 }/* add here a \ if you put your #if TEST_FLAG like the comment above */
 //put here your #endif //TEST_FLAG
 
@@ -40,7 +39,6 @@ if(!GlobalMockHelperInstance.expired() && GlobalMockHelperInstance.lock()->Conta
 const std::weak_ptr<SimpleMockHelper> MockHelperInstance = GetMockHelper();    \
 if(!MockHelperInstance.expired() && MockHelperInstance.lock()->ContainsMethodToMock(typeid(ClassMethodAddress).name()))    \
 {   \
-    using methodTraits = SimpleMockHelper::MethodTraits<decltype(ClassMethodAddress)>;    \
-    return MockHelperInstance.lock()->ExecuteMockMethod<ReturnType, methodTraits::classType>(ClassMethodAddress, ##__VA_ARGS__);    \
+    return MockHelperInstance.lock()->ExecuteMockMethod(ClassMethodAddress, ##__VA_ARGS__);    \
 }/* add here a \ if you put your #if TEST_FLAG like the comment above */
 //put here your #endif //TEST_FLAG
