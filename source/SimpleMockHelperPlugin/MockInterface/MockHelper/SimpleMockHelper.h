4bdd629a8b5df85da6e765316d39e47717cd1c99
alber <betomiku@gmail.com>
2025-05-25 18:19:43 +0200

    Replace cast invoke with std::invoke.

--- source/SimpleMockHelperPlugin/MockInterface/MockHelper/SimpleMockHelper.h
+++ source/SimpleMockHelperPlugin/MockInterface/MockHelper/SimpleMockHelper.h
@@ -55,7 +55,7 @@ inline ReturnType SimpleMockHelper::ExecuteMockMethod(ReturnType (ClassType::*Or
 
     std::function<ReturnType()>* ReplacingFunctionPointer = std::any_cast<std::function<ReturnType()>*>(ReplacingFunctionAddress);
 
-    return (*ReplacingFunctionPointer)();
+    return std::invoke(ReplacingFunctionPointer);
 }
 
 template <typename ReturnType, typename ClassType, typename... ArgumentTypes>
@@ -65,7 +65,7 @@ inline ReturnType SimpleMockHelper::ExecuteMockMethodWithArguments(ReturnType (C
 
     std::function<ReturnType(ArgumentTypes...)>* ReplacingFunctionPointer = std::any_cast<std::function<ReturnType(ArgumentTypes...)>*>(ReplacingFunctionAddress);
 
-    return (*ReplacingFunctionPointer)(ArgumentValues...);
+    return std::invoke(ReplacingFunctionPointer, std::forward<ArgumentTypes...>(ArgumentValues)...);
 }
 
 template <typename MethodPointer, typename... ArgumentTypes>
